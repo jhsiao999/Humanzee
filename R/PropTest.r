c4d12474ec599317ac413c43daeeeee9de9fc5f7
jhsiao999 <joyce.hsiao1@domail.com>
2015-08-13 14:13:52 -0500

    add PropTest.r

---
+++ R/PropTest.r
@@ -0,0 +1,24 @@
+#' Two-proportion z-test
+#' 
+#' This function compares two indepenent sample proportions.
+#' The null hypothesis is that the two proportions are equal. 
+#' 
+#' @param prop1 sample proportion
+#' @param prop2 sample proportion
+#' @param propAll 
+#' @param N1 Sample size of prop1
+#' @param N2 Sample size of prop2
+#'
+#' @keywords Humanzee
+#' 
+#' @export
+#' 
+#' @examples
+#' PropCorrTest()
+
+PropTest <- function(prop1, prop2, N1, N2) {
+  prop_all <- (prop1*N1 + prop2*N2)/(N1 + N2)
+  z <- (prop1 - prop2)/ sqrt( propAll*(1-propAll)*(1/N1 + 1/N2) )
+  pval <- 2*(1-pnorm(z))
+  list(z = z, pval = pval)
+}
